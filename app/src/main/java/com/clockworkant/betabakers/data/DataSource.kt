7f1beee655d6b3c49a829e0b88452bda989060ee
Alec Holmes <alec@clockworkant.com>
2019-06-18 12:14:51 +0100

    switch to Gson for json marshalling

--- app/src/main/java/com/clockworkant/betabakers/data/DataSource.kt
+++ app/src/main/java/com/clockworkant/betabakers/data/DataSource.kt
@@ -1,14 +1,15 @@
 package com.clockworkant.betabakers.data
 
-import com.beust.klaxon.Klaxon
+import com.google.gson.Gson
 import java.io.InputStream
+import java.io.InputStreamReader
 
-interface DataSource{
+interface DataSource {
     fun getCakes(): List<Cake>
 }
 
 class LocalDataSource(cakeInputStream: InputStream) : DataSource {
-    private val cakes: List<Cake> = Klaxon().parseArray(cakeInputStream)!!
+    private val cakes: List<Cake> = Gson().fromJson(InputStreamReader(cakeInputStream), Array<Cake>::class.java).toList()
 
     override fun getCakes() = cakes
 
